49f5d70bcbad88f4c719d5e3e0dbf0bb79e05a13
Vlad Shmyhlo <aion.stu@gmail.com>
2018-12-29 00:28:20 +0200

    Refactor

--- discriminator.py
+++ discriminator.py
@@ -7,16 +7,16 @@ class Convolutional(nn.Module):
         super().__init__()
 
         self.conv = nn.Sequential(
-            modules.ConvNorm2d(1, 32, 3, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            modules.ConvNorm2d(32, 64, 3, stride=2, padding=1),
+            modules.ConvNorm2d(1, 64, 3, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
 
             modules.ConvNorm2d(64, 128, 3, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(128, latent_size, 7))
+            modules.ConvNorm2d(128, 256, 3, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(256, latent_size, 7))
 
     def forward(self, input):
         input = self.conv(input)
--- generator.py
+++ generator.py
@@ -7,16 +7,16 @@ class Convolutional(nn.Module):
         super().__init__()
 
         self.conv = nn.Sequential(
-            modules.ConvTransposeNorm2d(latent_size, 128, 7),
+            modules.ConvTransposeNorm2d(latent_size, 256, 7),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            modules.ConvTransposeNorm2d(256, 128, 4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
 
             modules.ConvTransposeNorm2d(128, 64, 4, stride=2, padding=1),
             nn.LeakyReLU(0.2, inplace=True),
 
-            modules.ConvTransposeNorm2d(64, 32, 4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(32, 1, 3, padding=1),
+            nn.Conv2d(64, 1, 3, padding=1),
             nn.Tanh())
 
     def forward(self, input):
