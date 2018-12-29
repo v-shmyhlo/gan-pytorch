3bcf796407c08d0ad6cf10f62831cdf50c154215
Vlad Shmyhlo <aion.stu@gmail.com>
2018-12-29 14:08:57 +0200

    Refactor

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ import torchvision
 
 
 def make_grid(tensor):
-    return torchvision.utils.make_grid(tensor, nrow=math.round(math.sqrt(tensor.size(0))))
+    return torchvision.utils.make_grid(tensor, nrow=round(math.sqrt(tensor.size(0))))
